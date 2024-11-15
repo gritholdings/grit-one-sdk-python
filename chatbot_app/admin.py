5fa4947f80878d5cf94c498dd2a4d7fa2919a8b9
Edward Wong <edward@meetgrit.com>
2024-11-14 22:43:34 -0500

    add company name admin header

--- chatbot_app/admin.py
+++ chatbot_app/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import Prompt
 
+admin.site.site_header = "Your Company, Inc. Administration"
+
 class PromptAdmin(admin.ModelAdmin):
     list_display = ('name', 'status', 'internal_note')
     list_filter = ('status',)
